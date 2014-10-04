951485a4a722cde6392bf42db5df7d1504860ad1
Bryan Joseph <bryanjos@gmail.com>
2014-10-04 07:55:10 -0500

    Updated to 0.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PlugJwt.Mixfile do
 
   def project do
     [app: :plug_jwt,
-     version: "0.0.1",
+     version: "0.2.0",
      elixir: "~> 1.0.0",
      description: description,
      package: package,
@@ -33,8 +33,7 @@ defmodule PlugJwt.Mixfile do
      files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
      contributors: ["Bryan Joseph"],
      licenses: ["Apache 2.0"],
-     links: %{"GitHub" => "https://github.com/bryanjos/plug_jwt",
-              "Docs" => "https://github.com/bryanjos/plug_jwt"}
+     links: %{"GitHub" => "https://github.com/bryanjos/plug_jwt"}
     ]
   end
 end
